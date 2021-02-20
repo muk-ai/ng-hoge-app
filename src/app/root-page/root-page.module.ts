ab12d485b2ce39cc042ae255983fb1444c59310d
Kenta Mukai <r@muk.ai>
2021-02-20 15:02:35 +0000

    :sparkles: create root page

---
+++ src/app/root-page/root-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RootPageRoutingModule } from './root-page-routing.module';
+import { RootPageComponent } from './root-page/root-page.component';
+
+
+@NgModule({
+  declarations: [RootPageComponent],
+  imports: [
+    CommonModule,
+    RootPageRoutingModule
+  ]
+})
+export class RootPageModule { }
