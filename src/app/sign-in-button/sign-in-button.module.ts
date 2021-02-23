21405cd75190dbef3aec3e253c02198dce1cc6f1
Kenta Mukai <r@muk.ai>
2021-02-23 14:12:00 +0000

    :sparkles: add sign out button

--- src/app/sign-in-button/sign-in-button.module.ts
+++ src/app/sign-in-button/sign-in-button.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInWithGoogleButtonComponent } from './sign-in-with-google-button/sign-in-with-google-button.component';
+import { SignOutButtonComponent } from './sign-out-button/sign-out-button.component';
 
 @NgModule({
-  declarations: [SignInWithGoogleButtonComponent],
+  declarations: [SignInWithGoogleButtonComponent, SignOutButtonComponent],
   imports: [CommonModule],
-  exports: [SignInWithGoogleButtonComponent],
+  exports: [SignInWithGoogleButtonComponent, SignOutButtonComponent],
 })
 export class SignInButtonModule {}
