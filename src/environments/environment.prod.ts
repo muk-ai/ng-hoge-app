6334c774247ee3db3fd4e5113088486530a8a736
Kenta Mukai <r@muk.ai>
2021-02-23 10:56:00 +0000

    :gear: add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   apiHost: 'https://api.ng.hoge.app',
+  firebase: {
+    apiKey: 'AIzaSyDkN-ZuLQtlPcCJmqAjZSM2cl9dzhZ8RMs',
+    authDomain: 'ng-hoge-app.firebaseapp.com',
+    projectId: 'ng-hoge-app',
+    storageBucket: 'ng-hoge-app.appspot.com',
+    messagingSenderId: '816380104391',
+    appId: '1:816380104391:web:b7cf8ba1fc783ba649f8e2',
+    measurementId: 'G-28RC8Q2PEQ',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   apiHost: 'http://localhost:8000',
+  firebase: {
+    apiKey: 'AIzaSyDkN-ZuLQtlPcCJmqAjZSM2cl9dzhZ8RMs',
+    authDomain: 'ng-hoge-app.firebaseapp.com',
+    projectId: 'ng-hoge-app',
+    storageBucket: 'ng-hoge-app.appspot.com',
+    messagingSenderId: '816380104391',
+    appId: '1:816380104391:web:b7cf8ba1fc783ba649f8e2',
+    measurementId: 'G-28RC8Q2PEQ',
+  },
 };
 
 /*
