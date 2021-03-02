28bf9c94781c3ae338c8b7b33110902bfc957d9e
Kenta Mukai <r@muk.ai>
2021-03-02 15:29:03 +0000

    :sparkles: move get tasks

--- src/app/overview-page/overview-page/overview-page.component.ts
+++ src/app/overview-page/overview-page/overview-page.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+interface Task {
+  id: number;
+  description: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-overview-page',
@@ -6,7 +15,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview-page.component.scss'],
 })
 export class OverviewPageComponent implements OnInit {
-  constructor() {}
+  tasks$: Observable<Task[]> | null = null;
 
-  ngOnInit() {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url = `${environment.apiHost}/tasks`;
+    this.tasks$ = this.http.get<Task[]>(url);
+  }
 }
