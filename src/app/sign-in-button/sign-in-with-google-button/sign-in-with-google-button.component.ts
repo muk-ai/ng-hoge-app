35f0dc723972689aa966ffae75821c5f32f1a525
Kenta Mukai <r@muk.ai>
2021-03-28 21:38:54 +0900

    :sparkles: create user

--- src/app/sign-in-button/sign-in-with-google-button/sign-in-with-google-button.component.ts
+++ src/app/sign-in-button/sign-in-with-google-button/sign-in-with-google-button.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import firebase from 'firebase/app';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sign-in-with-google-button',
@@ -8,11 +10,40 @@ import firebase from 'firebase/app';
   styleUrls: ['./sign-in-with-google-button.component.scss'],
 })
 export class SignInWithGoogleButtonComponent implements OnInit {
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(private afAuth: AngularFireAuth, private http: HttpClient) {}
 
   ngOnInit() {}
 
-  signIn() {
-    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  async signIn() {
+    const result = await this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    if (result.user) {
+      const idToken = await result.user.getIdToken();
+      const url = `${environment.apiHost}/auth/me`;
+      this.http.get(url, this.authzHeaders(idToken)).subscribe(
+        _response => {
+          // NOTE: すでにユーザーは作成済なので何もしない
+        },
+        (err: HttpErrorResponse) => {
+          if (err.status === 404) {
+            this.http.post(url, '', this.authzHeaders(idToken)).subscribe(
+              _response => {
+                // NOTE: ユーザーを作成した時の処理
+              },
+              (_err: HttpErrorResponse) => {
+                alert('ユーザーの作成に失敗しました。');
+              }
+            );
+          }
+        }
+      );
+    }
+  }
+
+  private authzHeaders(idToken: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${idToken}`,
+    });
+    return { headers };
   }
 }
