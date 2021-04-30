ed2d2b5f3ef549d8c9e6dc52d480e4b49b311911
Kenta Mukai <r@muk.ai>
2021-04-30 21:11:32 +0900

    :sparkles: create task form

--- src/app/tasks-page/tasks-page.module.ts
+++ src/app/tasks-page/tasks-page.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TasksPageRoutingModule } from './tasks-page-routing.module';
 import { TasksPageComponent } from './tasks-page/tasks-page.component';
 
-
 @NgModule({
   declarations: [TasksPageComponent],
-  imports: [
-    CommonModule,
-    TasksPageRoutingModule
-  ]
+  imports: [CommonModule, TasksPageRoutingModule, ReactiveFormsModule],
 })
-export class TasksPageModule { }
+export class TasksPageModule {}
--- src/app/tasks-page/tasks-page/tasks-page.component.ts
+++ src/app/tasks-page/tasks-page/tasks-page.component.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { FormBuilder, Validators } from '@angular/forms';
 
 interface Task {
   id: number;
@@ -18,8 +19,11 @@ interface Task {
 })
 export class TasksPageComponent implements OnInit {
   tasks$: Observable<Task[]> | null = null;
+  newTaskForm = this.fb.group({
+    description: ['', Validators.required],
+  });
 
-  constructor(private http: HttpClient, private afAuth: AngularFireAuth) {}
+  constructor(private http: HttpClient, private afAuth: AngularFireAuth, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.afAuth.idToken.pipe(first()).subscribe(idToken => {
@@ -30,6 +34,21 @@ export class TasksPageComponent implements OnInit {
         });
         const url = `${environment.apiHost}/tasks`;
         this.tasks$ = this.http.get<Task[]>(url, { headers: headers });
+      }
+    });
+  }
+
+  onSubmit() {
+    this.afAuth.idToken.pipe(first()).subscribe(idToken => {
+      if (idToken) {
+        const headers = new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${idToken}`,
+        });
+        const url = `${environment.apiHost}/tasks`;
+        this.http
+          .post<Task>(url, this.newTaskForm.value, { headers: headers })
+          .subscribe();
       }
     });
   }
