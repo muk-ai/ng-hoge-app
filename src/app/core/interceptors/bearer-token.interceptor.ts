043ec5f86908effe27259f64372797f8e50607a5
Kenta Mukai <r@muk.ai>
2021-05-01 22:05:20 +0900

    :sparkles: make sure it is a request to my server

--- src/app/core/interceptors/bearer-token.interceptor.ts
+++ src/app/core/interceptors/bearer-token.interceptor.ts
@@ -3,6 +3,7 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } fro
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { first, switchMap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class BearerTokenInterceptor implements HttpInterceptor {
@@ -12,7 +13,7 @@ export class BearerTokenInterceptor implements HttpInterceptor {
     return this.afAuth.idToken.pipe(
       first(),
       switchMap(idToken => {
-        if (idToken) {
+        if (idToken && this.isApiRequest(request.url)) {
           const headers = new HttpHeaders({
             'Content-Type': 'application/json',
             Authorization: `Bearer ${idToken}`,
@@ -24,5 +25,9 @@ export class BearerTokenInterceptor implements HttpInterceptor {
         return next.handle(request);
       })
     );
+  }
+
+  private isApiRequest(targetUrl: string) {
+    return targetUrl.startsWith(environment.apiHost);
   }
 }
