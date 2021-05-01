4cb4147be924e38c3780ac526c3ba56d26fe505a
Kenta Mukai <r@muk.ai>
2021-05-01 21:43:58 +0900

    :sparkles: add bearer token

--- src/app/core/interceptors/bearer-token.interceptor.ts
+++ src/app/core/interceptors/bearer-token.interceptor.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { first, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class BearerTokenInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log('here is intercept()');
-    return next.handle(request);
+    return this.afAuth.idToken.pipe(
+      first(),
+      switchMap(idToken => {
+        if (idToken) {
+          const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${idToken}`,
+          });
+          request = request.clone({
+            headers: headers,
+          });
+        }
+        return next.handle(request);
+      })
+    );
   }
 }
