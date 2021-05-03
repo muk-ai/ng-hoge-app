0dfb25d04b4c8f448d5ab41350dfa8d7cbfb0c05
Kenta Mukai <r@muk.ai>
2021-05-03 21:43:24 +0900

    :gear: remove token

--- src/app/core/services/user-auth.service.ts
+++ src/app/core/services/user-auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import firebase from 'firebase/app';
 import { environment } from 'src/environments/environment';
 import { timeout } from 'rxjs/operators';
@@ -21,18 +21,14 @@ export class UserAuthService {
   async signInWithPopup(provider: firebase.auth.AuthProvider): Promise<AuthResult> {
     const result = await this.afAuth.signInWithPopup(provider);
     if (result.user) {
-      const idToken = await result.user.getIdToken();
       const url = `${environment.apiHost}/auth/me`;
       try {
-        await this.http
-          .get(url, { responseType: 'text', ...this.authzHeaders(idToken) })
-          .pipe(timeout(TIMEOUT_MSEC))
-          .toPromise();
+        await this.http.get(url, { responseType: 'text' }).pipe(timeout(TIMEOUT_MSEC)).toPromise();
         return { result: 'AlreadyCreated' };
       } catch (error) {
         if (error.status === 404) {
           try {
-            await this.http.post(url, '', this.authzHeaders(idToken)).pipe(timeout(TIMEOUT_MSEC)).toPromise();
+            await this.http.post(url, '').pipe(timeout(TIMEOUT_MSEC)).toPromise();
             return { result: 'Created' };
           } catch (error) {
             throw { result: 'Error', error };
@@ -44,13 +40,5 @@ export class UserAuthService {
     }
 
     throw { result: 'Error' };
-  }
-
-  private authzHeaders(idToken: string) {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${idToken}`,
-    });
-    return { headers };
   }
 }
