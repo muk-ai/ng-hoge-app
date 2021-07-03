fd18d23fdf3560759d5c3493dc9c1b7d65324a17
Kenta Mukai <r@muk.ai>
2021-07-03 22:50:45 +0900

    :sparkles: check existence of the user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,8 +9,38 @@ import { environment } from '../environments/environment';
 import { BearerTokenInterceptor } from './core/interceptors/bearer-token.interceptor';
 
 import { of } from 'rxjs';
-function initializeApp() {
-  return () => of(null);
+import { catchError, first, mergeMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+function initializeApp(http: HttpClient, afAuth: AngularFireAuth) {
+  return () => {
+    return afAuth.idToken.pipe(
+      first(),
+      mergeMap(idToken => {
+        if (!idToken) {
+          return of();
+        }
+
+        // NOTE: dbにUserが存在するか確認している。
+        //       firebaseではログインしていて、自前のapiにはUserが存在していない不整合な状態の時はfirebaseでログアウトする。
+        //       次にfirebaseでログインした時にUserが作られるので不整合は解消される。
+        const url = `${environment.apiHost}/auth/me`;
+        return http.get(url, { responseType: 'text' }).pipe(
+          mergeMap(_response => {
+            // NOTE: Userは存在しているので問題なし
+            return of();
+          }),
+          catchError((response: HttpErrorResponse) => {
+            console.log(response);
+            if (response.status === 404) {
+              // NOTE: 不整合な状態なのでログアウトする
+              afAuth.signOut();
+            }
+            return of();
+          })
+        );
+      })
+    );
+  };
 }
 
 @NgModule({
@@ -20,6 +50,7 @@ function initializeApp() {
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
+      deps: [HttpClient, AngularFireAuth],
       multi: true,
     },
     {
