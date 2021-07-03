11101cc49083b67eeacb2d1c11a7c374aa08fe8e
Kenta Mukai <r@muk.ai>
2021-07-03 21:20:19 +0900

    :sparkles: try to use APP_INITIALIZER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire';
@@ -8,10 +8,26 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { BearerTokenInterceptor } from './core/interceptors/bearer-token.interceptor';
 
+import { of } from 'rxjs';
+function initializeApp() {
+  return () => of(null);
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, AngularFireModule.initializeApp(environment.firebase)],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: BearerTokenInterceptor, multi: true }],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BearerTokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
