f745b1140dd0edc07bdea3e01a4576e936b37e22
Kenta Mukai <r@muk.ai>
2021-08-07 17:58:00 +0900

    :sparkles: impl delete my account

--- src/app/sign-in-button/delete-my-account-button/delete-my-account-button.component.ts
+++ src/app/sign-in-button/delete-my-account-button/delete-my-account-button.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserAuthService } from 'src/app/core/services/user-auth.service';
 
 @Component({
   selector: 'app-delete-my-account-button',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-my-account-button.component.scss'],
 })
 export class DeleteMyAccountButtonComponent implements OnInit {
-  constructor() {}
+  constructor(private userAuth: UserAuthService, private router: Router) {}
 
   ngOnInit() {}
 
-  deleteMyAccount() {}
+  deleteMyAccount() {
+    this.userAuth
+      .deleteMyAccount()
+      .then(() => {
+        this.router.navigate(['/']);
+      })
+      .catch(_ => {
+        alert('失敗しました');
+      });
+  }
 }
