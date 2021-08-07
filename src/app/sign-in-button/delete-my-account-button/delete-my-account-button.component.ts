346a23f3917646e1df18c761a4ca72ebbca4c2b8
Kenta Mukai <r@muk.ai>
2021-08-07 13:03:25 +0900

    :sparkles: create <delete-my-account-button>

---
+++ src/app/sign-in-button/delete-my-account-button/delete-my-account-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-my-account-button',
+  templateUrl: './delete-my-account-button.component.html',
+  styleUrls: ['./delete-my-account-button.component.scss'],
+})
+export class DeleteMyAccountButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  deleteMyAccount() {}
+}
