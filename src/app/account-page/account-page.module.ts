4b9d41e504a21bcf9fce8982783b423e8ac3fb61
Kenta Mukai <r@muk.ai>
2021-08-07 12:53:15 +0900

    :sparkles: move sign out button

--- src/app/account-page/account-page.module.ts
+++ src/app/account-page/account-page.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AccountPageRoutingModule } from './account-page-routing.module';
 import { AccountPageComponent } from './account-page/account-page.component';
-
+import { SignInButtonModule } from '../sign-in-button/sign-in-button.module';
 
 @NgModule({
-  declarations: [
-    AccountPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AccountPageRoutingModule
-  ]
+  declarations: [AccountPageComponent],
+  imports: [CommonModule, AccountPageRoutingModule, SignInButtonModule],
 })
-export class AccountPageModule { }
+export class AccountPageModule {}
