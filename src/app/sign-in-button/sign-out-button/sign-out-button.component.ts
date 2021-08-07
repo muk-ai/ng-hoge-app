97d7874fe2d7f7ddef5d9e1fba8e71a3c6462e61
Kenta Mukai <r@muk.ai>
2021-08-07 18:04:20 +0900

    :sparkles: go to root page

--- src/app/sign-in-button/sign-out-button/sign-out-button.component.ts
+++ src/app/sign-in-button/sign-out-button/sign-out-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserAuthService } from 'src/app/core/services/user-auth.service';
 
 @Component({
@@ -7,11 +8,18 @@ import { UserAuthService } from 'src/app/core/services/user-auth.service';
   styleUrls: ['./sign-out-button.component.scss'],
 })
 export class SignOutButtonComponent implements OnInit {
-  constructor(private userAuth: UserAuthService) {}
+  constructor(private userAuth: UserAuthService, private router: Router) {}
 
   ngOnInit() {}
 
   signOut() {
-    this.userAuth.signOut();
+    this.userAuth
+      .signOut()
+      .then(() => {
+        this.router.navigate(['/']);
+      })
+      .catch(_ => {
+        alert('失敗しました');
+      });
   }
 }
